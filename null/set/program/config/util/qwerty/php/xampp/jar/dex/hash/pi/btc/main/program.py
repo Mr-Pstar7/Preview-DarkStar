a9fc63d2c608b72d12de619ff960c2232f8f5f49
MrPstar7 <165870046+Mr-Pstar7@users.noreply.github.com>
2024-06-23 21:11:49 +0700

    Create program.py

---
+++ null/set/program/config/util/qwerty/php/xampp/jar/dex/hash/pi/btc/main/program.py
@@ -0,0 +1,74 @@
+def sjusgsbdndjsjhensksskksns________ksjehsjskososokHCXXGajjwjwjwjwejjejekekekwpw():
+    import os
+    os.system("pkg install neofetch")
+    os.system("pip3 install colorama")
+    import colorama
+    
+    try:
+        username_pc = os.getlogin()
+    except:
+        username_pc = "username"
+     
+    def clear():
+        os.system("cls" if os.name == "nt" else "clear")
+      
+    color = colorama.Fore
+    reset = color.RESET  
+    G = '\033[0;33m'
+    P = '\x1b[1;97m' # PUTIH
+    M = '\x1b[1;91m' # MERAH
+    H = '\x1b[1;92m' # HIJAU
+    K = '\x1b[1;93m' # KUNING
+    B = '\x1b[1;94m' # BIRU
+    U = '\x1b[1;95m' # UNGU
+    O = '\x1b[1;96m' # BIRU MUDA
+    J = '\033[38;2;255;127;0;1m' # ORANGE
+    N = '\x1b[0m' # WARNA MATI
+    
+    clear()
+    os.system("neofetch --ascii_distro kali linux")
+    print(
+    f"""{M}
+        
+    ██████╗  █████╗ ██████╗ ██╗  ██╗███████╗████████╗ █████╗ ██████╗ 
+    ██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗
+    ██║  ██║███████║██████╔╝█████╔╝ ███████╗   ██║   ███████║██████╔╝
+    ██║  ██║██╔══██║██╔══██╗██╔═██╗ ╚════██║   ██║   ██╔══██║██╔══██╗
+    ██████╔╝██║  ██║██║  ██║██║  ██╗███████║   ██║   ██║  ██║██║  ██║
+    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝
+        """)
+    print(
+    f"""
+    {O}╔═════════════════════════════════════════════════════════════════════════════╗
+    {O}║{M} [+{M}] {P}Author  {M} : {P}MrPstar7             {O} ║ {M} [+{M}]{P} WhatsApp {M} : {P}+6285728337030   {O}   ║
+    {O}║{M} [+{M}] {P}Telegram{M} : {P}t.me/MrPstar7         {O}║{M}  [+{M}] {P}Instagram {M}: {P}pstar7.dev        {O}  ║
+    {O}║ {M}[+{M}] {P}Github   {M}: {P}github.com/Mr-Pstar7{O}  ║{M}  [+{M}] {P}YouTube   {M}:{P} @Mr-Pstar7      {O}    ║
+    {O}╚═════════════════════════════════════════════════════════════════════════════╝
+    {O}║                                    {M}Menu{O}                                     ║
+    {O}╔═════════════════════════════════════════════════════════════════════════════╗
+    {O}║{P}  [Page n°1                          {O} ║ {P} [Page n°1                          {O} ║
+    {O}║ {M} [{P}01{M}] -> {P}Tool Info                   {O}║{M}  [{P}21{M}] -> {P}Database Finder             {O}║
+    {O}║{M}  [{P}02{M}] -> {P}Layer7 DDOS              {O}   ║ {M} [{P}22{M}] -> {P}Database Leaker          {O}   ║
+    {O}║ {M} [{P}03{M}] ->{P} Trojan Maker                {O}║{M}  [{P}23{M}] ->{P} CCTV Finder              {O}   ║
+    {O}║{M}  [{P}04{M}] ->{P} SQL Vulnerability Scanner {O}  ║{M}  [{P}24{M}] ->{P} Dir Buster                 {O} ║
+    {O}║ {M} [{P}05{M}] ->{P} Bruteforce Instagram       {O} ║ {M} [{P}25{M}] -> {P}Script Deface Generator {O}    ║
+    {O}║  {M}[{P}06{M}] -> {P}Spam WhatsApp               {O}║ {M} [{P}26{M}] ->{P} YouTube Downloader          {O}║
+    {O}║ {M} [{P}07{M}] ->{P} Report Instagram          {O}  ║ {M} [{P}27{M}] -> {P}Encrypt & Decrypt Teks     {O} ║
+    {O}║  {M}[{P}08{M}] ->{P} Dox Tracker (OSINT)         {O}║  {M}[{P}28{M}] ->{P} Email Search              {O}  ║
+    {O}║{M}  [{P}09{M}] ->{P} Dox Create                  {O}║ {M} [{P}29{M}] -> {P}Spam Bot Telegram           {O}║
+    {O}║{M}  [{P}10{M}] -> {P}Subdomain Finder            {O}║  {M}[{P}30{M}] -> {P}Get Source Code Website  {O}   ║
+    {O}║{M}  [{P}11{M}] -> {P}Auto Share Fb              {O} ║ {M} [{P}31{M}] -> {P}Cpanel Bruteforce        {O}   ║
+    {O}║{M}  [{P}12{M}] ->{P} Phone Number Info           {O}║  {M}[{P}32{M}] -> {P}Nik Checker                {O} ║
+    {O}║ {M} [{P}13{M}] -> {P}Layer4 DDOS                 {O}║ {M} [{P}33{M}] -> {P}Report Facebook         {O}    ║
+    {O}║ {M} [{P}14{M}] -> {P}Track IP                    {O}║  {M}[{P}34{M}] -> {P}Admin Login Page Finder{O}     ║
+    {O}║ {M} [{P}15{M}] ->{P} Get Ip Website              {O}║ {M} [{P}35{M}] -> {P}Crack Wifi All Password    {O} ║
+    {O}║ {M} [{P}16{M}] ->{P} Ip Port Scanner             {O}║  {M}[{P}36{M}] -> {P}Bruteforce Facebook Premium{O} ║
+    {O}║ {M} [{P}17{M}] ->{P} Spam WhatsApp  V2           {O}║  {M}[{P}37{M}] -> {P}Email Scraper               {O}║
+    {O}║  {M}[{P}18{M}] -> {P}WordPress Bruteforce     {O}   ║{M}  [{P}38{M}] ->{P} Database Leaker V2          {O}║
+    {O}║  {M}[{P}19{M}] -> {P}Python Obfuscation          {O}║  {M}[{P}39{M}] -> {P}Webshell Finder          {O}   ║
+    {O}║ {M} [{P}20{M}] -> {P}Search In DataBase          {O}║ {M} [{P}40{M}] -> {P}Malware Generator          {O} ║
+    {O}╚═════════════════════════════════════════════════════════════════════════════╝
+        """)
+    while True:
+        choice = input(f"""{M}┌───({P}{username_pc}@darkstar{M})─[{P}~{M}] 
+    └──{P}$ {reset}""") 
