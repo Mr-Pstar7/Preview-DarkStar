838f999e5c0a8066fcfb96ec38b73e0e89c39bd7
MrPstar7 <165870046+Mr-Pstar7@users.noreply.github.com>
2024-10-27 20:34:37 +0700

    Update program.py

--- null/set/program/config/util/qwerty/php/xampp/jar/dex/hash/pi/btc/main/program.py
+++ null/set/program/config/util/qwerty/php/xampp/jar/dex/hash/pi/btc/main/program.py
@@ -39,35 +39,35 @@ def sjusgsbdndjsjhensksskksns________ksjehsjskososokHCXXGajjwjwjwjwejjejekekekwp
         """)
     print(
     f"""
-{O}╔═════════════════════════════════════════════════════════════════════════════╗
-{O}║{M} [+{M}] {P}Author  {M} : {P}MrPstar7             {O} ║ {M} [+{M}]{P} WhatsApp {M} : {P}+6285728337030   {O}   ║
-{O}║{M} [+{M}] {P}Telegram{M} : {P}t.me/MrPstar7         {O}║{M}  [+{M}] {P}Instagram {M}: {P}pstar7.dev        {O}  ║
-{O}║ {M}[+{M}] {P}Github   {M}: {P}github.com/Mr-Pstar7{O}  ║{M}  [+{M}] {P}YouTube   {M}:{P} @Mr-Pstar7      {O}    ║
-{O}╚═════════════════════════════════════════════════════════════════════════════╝
-{O}║                                    {M}Menu{O}                                     ║
-{O}╔═════════════════════════════════════════════════════════════════════════════╗
-{O}║{P}  [Page n°1                          {O} ║ {P} [Page n°1                          {O} ║
-{O}║ {M} [{P}01{M}] -> {P}Tool Info                   {O}║{M}  [{P}21{M}] -> {P}Database Finder             {O}║
-{O}║{M}  [{P}02{M}] -> {P}Layer7 DDOS              {O}   ║ {M} [{P}22{M}] -> {P}Database Leaker          {O}   ║
-{O}║ {M} [{P}03{M}] ->{P} Trojan Maker                {O}║{M}  [{P}23{M}] ->{P} CCTV Finder              {O}   ║
-{O}║{M}  [{P}04{M}] ->{P} SQL Vulnerability Scanner {O}  ║{M}  [{P}24{M}] ->{P} Dir Buster                 {O} ║
-{O}║ {M} [{P}05{M}] ->{P} Bruteforce Instagram       {O} ║ {M} [{P}25{M}] -> {P}Script Deface Generator {O}    ║
-{O}║  {M}[{P}06{M}] -> {P}Spam WhatsApp               {O}║ {M} [{P}26{M}] ->{P} YouTube Downloader          {O}║
-{O}║ {M} [{P}07{M}] ->{P} Report Instagram          {O}  ║ {M} [{P}27{M}] -> {P}Hash Cracker               {O} ║
-{O}║  {M}[{P}08{M}] ->{P} Dox Tracker (OSINT)         {O}║  {M}[{P}28{M}] ->{P} Email Search              {O}  ║
-{O}║{M}  [{P}09{M}] ->{P} Dox Create                  {O}║ {M} [{P}29{M}] -> {P}Spam Bot Telegram           {O}║
-{O}║{M}  [{P}10{M}] -> {P}Subdomain Finder            {O}║  {M}[{P}30{M}] -> {P}Get Source Code Website  {O}   ║
-{O}║{M}  [{P}11{M}] -> {P}Auto Share Fb              {O} ║ {M} [{P}31{M}] -> {P}Cpanel Bruteforce        {O}   ║
-{O}║{M}  [{P}12{M}] ->{P} Phone Number Info           {O}║  {M}[{P}32{M}] -> {P}Nik Checker                {O} ║
-{O}║ {M} [{P}13{M}] -> {P}Layer4 DDOS                 {O}║ {M} [{P}33{M}] -> {P}Report Facebook         {O}    ║
-{O}║ {M} [{P}14{M}] -> {P}Track IP                    {O}║  {M}[{P}34{M}] -> {P}Admin Login Page Finder{O}     ║
-{O}║ {M} [{P}15{M}] ->{P} Get Ip Website              {O}║ {M} [{P}35{M}] -> {P}Crack Wifi All Password    {O} ║
-{O}║ {M} [{P}16{M}] ->{P} Ip Port Scanner             {O}║  {M}[{P}36{M}] -> {P}Bruteforce Facebook Premium{O} ║
-{O}║ {M} [{P}17{M}] ->{P} Damin Grabber               {O}║  {M}[{P}37{M}] -> {P}Email Scraper               {O}║
-{O}║  {M}[{P}18{M}] -> {P}WordPress Bruteforce     {O}   ║{M}  [{P}38{M}] ->{P} Database Leaker V2          {O}║
-{O}║  {M}[{P}19{M}] -> {P}Python Obfuscation          {O}║  {M}[{P}39{M}] -> {P}Webshell Finder          {O}   ║
-{O}║ {M} [{P}20{M}] -> {P}Search In DataBase          {O}║ {M} [{P}40{M}] -> {P}Malware Generator          {O} ║
-{O}╚═════════════════════════════════════════════════════════════════════════════╝
+    {O}╔═════════════════════════════════════════════════════════════════════════════╗
+    {O}║{M} [+{M}] {P}Author  {M} : {P}MrPstar7             {O} ║ {M} [+{M}]{P} WhatsApp {M} : {P}+6285728337030   {O}   ║
+    {O}║{M} [+{M}] {P}Telegram{M} : {P}t.me/MrPstar7         {O}║{M}  [+{M}] {P}Instagram {M}: {P}pstar7.dev        {O}  ║
+    {O}║ {M}[+{M}] {P}Github   {M}: {P}github.com/Mr-Pstar7{O}  ║{M}  [+{M}] {P}YouTube   {M}:{P} @Mr-Pstar7      {O}    ║
+    {O}╚═════════════════════════════════════════════════════════════════════════════╝
+    {O}║                                    {M}Menu{O}                                     ║
+    {O}╔═════════════════════════════════════════════════════════════════════════════╗
+    {O}║{P}  [Page n°1                          {O} ║ {P} [Page n°1                          {O} ║
+    {O}║ {M} [{P}01{M}] -> {P}Tool Info                   {O}║{M}  [{P}21{M}] -> {P}Database Finder             {O}║
+    {O}║{M}  [{P}02{M}] -> {P}Layer7 DDOS              {O}   ║ {M} [{P}22{M}] -> {P}Database Leaker          {O}   ║
+    {O}║ {M} [{P}03{M}] ->{P} Trojan Maker                {O}║{M}  [{P}23{M}] ->{P} CCTV Finder              {O}   ║
+    {O}║{M}  [{P}04{M}] ->{P} SQL Vulnerability Scanner {O}  ║{M}  [{P}24{M}] ->{P} Dir Buster                 {O} ║
+    {O}║ {M} [{P}05{M}] ->{P} Bruteforce Instagram       {O} ║ {M} [{P}25{M}] -> {P}Script Deface Generator {O}    ║
+    {O}║  {M}[{P}06{M}] -> {P}Spam WhatsApp               {O}║ {M} [{P}26{M}] ->{P} YouTube Downloader          {O}║
+    {O}║ {M} [{P}07{M}] ->{P} Report Instagram          {O}  ║ {M} [{P}27{M}] -> {P}Hash Cracker               {O} ║
+    {O}║  {M}[{P}08{M}] ->{P} Dox Tracker (OSINT)         {O}║  {M}[{P}28{M}] ->{P} Email Search              {O}  ║
+    {O}║{M}  [{P}09{M}] ->{P} Dox Create                  {O}║ {M} [{P}29{M}] -> {P}Spam Bot Telegram           {O}║
+    {O}║{M}  [{P}10{M}] -> {P}Subdomain Finder            {O}║  {M}[{P}30{M}] -> {P}Get Source Code Website  {O}   ║
+    {O}║{M}  [{P}11{M}] -> {P}Cpanel Checker             {O} ║ {M} [{P}31{M}] -> {P}Cpanel Bruteforce        {O}   ║
+    {O}║{M}  [{P}12{M}] ->{P} Wp Login Checker            {O}║  {M}[{P}32{M}] -> {P}Nik Checker                {O} ║
+    {O}║ {M} [{P}13{M}] -> {P}Layer4 DDOS                 {O}║ {M} [{P}33{M}] -> {P}Report Facebook         {O}    ║
+    {O}║ {M} [{P}14{M}] -> {P}Track IP                    {O}║  {M}[{P}34{M}] -> {P}Admin Login Page Finder{O}     ║
+    {O}║ {M} [{P}15{M}] ->{P} Get Ip Website              {O}║ {M} [{P}35{M}] -> {P}Crack Wifi All Password    {O} ║
+    {O}║ {M} [{P}16{M}] ->{P} Ip & Domain Port Scanner    {O}║  {M}[{P}36{M}] -> {P}Bruteforce Facebook Premium{O} ║
+    {O}║ {M} [{P}17{M}] ->{P} Domain Grabber              {O}║  {M}[{P}37{M}] -> {P}Email Scraper               {O}║
+    {O}║  {M}[{P}18{M}] -> {P}WordPress Bruteforce     {O}   ║{M}  [{P}38{M}] ->{P} Database Leaker V2          {O}║
+    {O}║  {M}[{P}19{M}] -> {P}Python Obfuscation          {O}║  {M}[{P}39{M}] -> {P}Webshell Finder          {O}   ║
+    {O}║ {M} [{P}20{M}] -> {P}Cms Detector                {O}║ {M} [{P}40{M}] -> {P}Malware Generator          {O} ║
+    {O}╚═════════════════════════════════════════════════════════════════════════════╝
         """)
     while True:
         choice = input(f"""{M}┌───({P}{username_pc}@darkstar{M})─[{P}~{M}] 
